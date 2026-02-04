d0169f2cbc5fb1baa54b4d8a1e284077b4f3451d
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-02-04 09:13:49 +0300

    Implement power calculation and modulo operation
    Calculate power and result modulo m.

---
+++ ModPower.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+m = int(input())
+power = pow(a,b)
+result = power % m
+print(power)
+print(result)
