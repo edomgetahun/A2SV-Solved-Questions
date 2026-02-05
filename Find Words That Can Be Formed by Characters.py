9867d53d02476dfdfcbcd9364192c6965f36bd16
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-02-05 17:15:55 +0300

    Add countCharacters method to Solution class
    Implement countCharacters method to count valid words.

---
+++ Find Words That Can Be Formed by Characters.py
@@ -0,0 +1,22 @@
+from collections import Counter
+class Solution(object):
+    def countCharacters(self, words, chars):
+        """
+        :type words: List[str]
+        :type chars: str
+        :rtype: int
+        """
+        countC = Counter(chars)
+        ans = 0
+        for word in words:
+            countW = Counter(word)
+            for ch in word:
+                if countW[ch] > countC[ch]:
+                    break
+            else:
+                ans += len(word)
+        return ans
+
+
+
+        
