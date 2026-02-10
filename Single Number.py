8a363b8050fd8c6793927bc0a6d41803e02bf856
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-02-10 10:06:40 +0300

    Implement singleNumber method to find unique number

---
+++ Single Number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        single_num = 0
+        for num in nums:
+            single_num ^= num
+        return single_num
+            
