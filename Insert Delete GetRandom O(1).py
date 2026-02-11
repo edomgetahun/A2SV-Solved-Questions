1fbc936f47f1a12ddf7b38e240ef237417db5309
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-02-11 09:23:17 +0300

    Add RandomizedSet class with basic operations
    Implement a RandomizedSet class with insert, remove, and getRandom methods.

---
+++ Insert Delete GetRandom O(1).py
@@ -0,0 +1,44 @@
+import random
+class RandomizedSet(object):
+
+    def __init__(self):
+        self.randomSet = set()
+
+    def insert(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.randomSet:
+            self.randomSet.add(val)
+            return True
+        else:
+            return False 
+        
+
+    def remove(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.randomSet:
+            self.randomSet.remove(val)
+            return True
+        else:
+            return False
+
+  
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        result = random.choice(list(self.randomSet))
+        return result
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
