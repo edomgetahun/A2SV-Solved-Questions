33dab0c142e6d3d8477682b14e38e0152a82f3c0
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-03-06 12:23:57 +0300

    Add solution for Range Sum Query 2D - Immutable

--- leetcode/0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ leetcode/0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -5,11 +5,8 @@ class NumMatrix:
         m = len(matrix[0])
         self.prefix = [[0] * (m + 1) for r in range(n+1)]
         for r in range(n):
-            curr = 0
             for c in range(m):
-                curr += matrix[r][c] 
-                above = self.prefix[r][c+1]
-                self.prefix[r+1][c+1] = curr + above 
+                self.prefix[r+1][c+1] += matrix[r][c] + self.prefix[r][c+1] + self.prefix[r+1][c] -self.prefix[r][c]  
         
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         row1, col1 , row2, col2 = row1+1, col1+1 , row2+1, col2+1
