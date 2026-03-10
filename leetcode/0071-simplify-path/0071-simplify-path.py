8df09ad9875981be7adc2b2213c2434900e1f1ef
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-03-10 14:19:40 +0300

    Add solution for Simplify Path

---
+++ leetcode/0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,14 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path = path.split("/")
+        stack = []
+        
+        for word in path:
+            if word == "." or word == "":
+                continue
+            elif word == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(word)
+        return "/" + "/".join(stack)
