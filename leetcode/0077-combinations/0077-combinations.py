fc449e996f4f87d4db1cf957ef3d199ccdd22532
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-03-25 21:59:06 +0300

    Add solution for Combinations

--- leetcode/0077-combinations/0077-combinations.py
+++ leetcode/0077-combinations/0077-combinations.py
@@ -1,24 +1,32 @@
 class Solution:
-    res = []
     def combine(self, n: int, k: int) -> List[List[int]]:
-        # initialy start = 1 and comb = []
-        # if len==k res.append(comp) and just return
-        # for num in range(start, n+1)
-        # comb.append(n)
-        # backtrack(num + 1, comb)
-        # comb.pop()
-        
-        def backtrack(num, comb):
+        # option 1
+        def backtrack(start, comb):
             if len(comb) == k:
                 res.append(comb[:])
                 return
-            if num > n:
-                return
-            comb.append(num)
-            backtrack(num+1, comb)
-            comb.pop()
-            backtrack(num+1, comb)
-        
+            for num in range(start, n+1):
+                comb.append(num)
+                backtrack(num + 1, comb)
+                comb.pop()
+
         res = []
         backtrack(1, [])
         return res
+
+
+            # option 2
+        # def backtrack(num, comb):
+        #     if len(comb) == k:
+        #         res.append(comb[:])
+        #         return
+        #     if num > n:
+        #         return
+        #     comb.append(num)
+        #     backtrack(num+1, comb)
+        #     comb.pop()
+        #     backtrack(num+1, comb)
+        
+        # res = []
+        # backtrack(1, [])
+        # return res
