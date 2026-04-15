869e8ec189add03f1210582af7c26f09095dab4c
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-04-15 23:05:18 +0300

    Add solution for Set Mismatch

--- leetcode/0645-set-mismatch/0645-set-mismatch.py
+++ leetcode/0645-set-mismatch/0645-set-mismatch.py
@@ -1,13 +1,17 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
         res = []
         count = Counter(nums)
         for num in count:
             if count[num] == 2:
-                res.append(num)
-        for num in range(1, len(nums)+1):
-            if num not in nums:
-                res.append(num)
-        return res
+                dup = num
+                res.append(dup)
 
+        expeted = (n*(n +1))//2
+        curr = sum(nums)
+        mis = expeted - (curr - dup)
+        res.append(mis)
+        return res
+        
 
