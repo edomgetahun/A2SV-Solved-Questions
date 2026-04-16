7c6ca90118d9ac9ff52e569246bdc2b788469050
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-04-16 12:17:49 +0300

    Add solution for Non-decreasing Subsequences

--- leetcode/0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ leetcode/0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -1,18 +1,16 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        res = set()
         def bk(start, comb):
             if len(comb) >= 2:
-                res.append(comb[:])
-            
-            seen = set()
+                res.add(tuple(comb[:]))
+                
             for i in range(start, len(nums)):
-                if nums[i] not in seen:
-                    if comb and nums[i] < comb[-1]:
-                        continue 
-                    seen.add(nums[i])
-                    comb.append(nums[i])
-                    bk(i+1, comb)
-                    comb.pop()
+                if comb and comb[-1] > nums[i]:
+                    continue
+                comb.append(nums[i])
+                bk(i+1, comb)
+                comb.pop()
         bk(0, [])
-        return res
+        return list(res)
+
