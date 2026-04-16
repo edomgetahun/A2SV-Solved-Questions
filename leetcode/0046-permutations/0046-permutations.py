b21ca2d4be3fded88e833b91e486a7e805a2c019
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-04-16 12:41:38 +0300

    Add solution for Permutations

--- leetcode/0046-permutations/0046-permutations.py
+++ leetcode/0046-permutations/0046-permutations.py
@@ -1,16 +1,37 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        n = max(nums)
         res = []
-        def backtrack(curr):
-            if len(curr) == len(nums):
-                res.append(curr[:])
+        def bk(comb):
+            if len(comb) == len(nums):
+                res.append(comb[:])
                 return
-            for num in nums:
-                if num not in curr:
-                    curr.append(num)
-                    backtrack(curr)
-                    curr.pop()
-                
-        backtrack([])
+            for i in range(len(nums)):
+                if nums[i] in comb:
+                    continue
+                comb.append(nums[i])
+                bk(comb)
+                comb.pop()
+        bk([])
         return res
+
+
+
+
+
+
+
+
+
+        # res = []
+        # def backtrack(curr):
+        #     if len(curr) == len(nums):
+        #         res.append(curr[:])
+        #         return
+        #     for num in nums:
+        #         if num not in curr:
+        #             curr.append(num)
+        #             backtrack(curr)
+        #             curr.pop()
+                
+        # backtrack([])
+        # return res
