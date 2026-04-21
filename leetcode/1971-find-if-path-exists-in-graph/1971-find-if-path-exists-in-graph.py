e8eadae124f48aaa17f252594267b6ede9bf69a0
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-04-21 19:10:52 +0300

    Add solution for Find if Path Exists in Graph

--- leetcode/1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ leetcode/1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,35 +1,53 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = defaultdict(list)
-        for node1, node2 in edges:
-            graph[node1].append(node2)
-            graph[node2].append(node1)
+        # graph = defaultdict(list)
+        # for node1, node2 in edges:
+        #     graph[node1].append(node2)
+        #     graph[node2].append(node1)
         
-        vis = set() 
-        # def dfs(node, vis):         # this code also return when the visted all
+        # vis = set() 
+        # # def dfs(node, vis):         # this code also return when the visted all
+        # #     if node == destination:
+        # #         return True
+        # #     vis.add(node)
+        # #     for negh in graph[node]:
+        # #         if negh not in vis:
+        # #             found = dfs(negh, vis)
+        # #             if found :
+        # #                 return True
+        # #     return False  
+        # # return dfs(source, vis)
+        # stack = [source]
+        # while stack:
+        #     node = stack.pop()
         #     if node == destination:
         #         return True
+            
+        #     if node in vis:
+        #         continue 
         #     vis.add(node)
         #     for negh in graph[node]:
         #         if negh not in vis:
-        #             found = dfs(negh, vis)
-        #             if found :
-        #                 return True
-        #     return False  
-        # return dfs(source, vis)
-        stack = [source]
-        while stack:
-            node = stack.pop()
+        #            stack.append(negh)
+        # return False
+        graph = [[] for _ in range(n)]
+        for node1, node2 in edges:
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+        vis = set()
+
+        def dfs(node, vis):
             if node == destination:
                 return True
-            
-            if node in vis:
-                continue 
             vis.add(node)
             for negh in graph[node]:
                 if negh not in vis:
-                   stack.append(negh)
-        return False
+                    found = dfs(negh, vis)
+                    if found:
+                        return True
+            return False
+        return dfs(source, vis)
+
                 
 
 
