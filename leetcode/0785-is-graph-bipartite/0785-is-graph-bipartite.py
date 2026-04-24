ab9e83ceb5bdde2dfe6a270d1aaca74596e52426
Edom Getahun <edom.getahun@aastustudent.edu.et>
2026-04-24 13:30:45 +0300

    Add solution for Is Graph Bipartite?

--- leetcode/0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ leetcode/0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,26 +1,22 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n = len(graph)
-        color = ["white"] * n
-        res = True
+        color = ["white"] * n 
         def dfs(node):
-            nonlocal res
             for negh in graph[node]:
                 if color[negh] == "white":
                     if color[node] == "red":
                         color[negh] = "blue"
                     else:
                         color[negh] = "red"
-                    dfs(negh)
-                else:
-                    if color[negh] == color[node]:
-                        res = False
-                        return # stop dfs immediatly
-        
+                    if not dfs(negh):
+                        return False 
+                elif color[negh] == color[node]:
+                    return  False
+            return True 
         for node in range(n):
             if color[node] == "white":
-                color[node] = "red" # start with red
-                dfs(node)
-                if res == False:
-                    break
-        return res
+                color[node] = "red"   
+                if not dfs(node):
+                    return False
+        return True
